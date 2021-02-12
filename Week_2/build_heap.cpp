e4ebfcff6eec57eee49cf7e07550c95b216adfeb
eltoncyc <mreltonchan@gmail.com>
2021-02-11 23:45:21 -0800

    add heap starter

---
+++ Week_2/build_heap.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::swap;
+using std::pair;
+using std::make_pair;
+
+class HeapBuilder {
+ private:
+  vector<int> data_;
+  vector< pair<int, int> > swaps_;
+
+  void WriteResponse() const {
+    cout << swaps_.size() << "\n";
+    for (int i = 0; i < swaps_.size(); ++i) {
+      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
+    }
+  }
+
+  void ReadData() {
+    int n;
+    cin >> n;
+    data_.resize(n);
+    for(int i = 0; i < n; ++i)
+      cin >> data_[i];
+  }
+
+  void GenerateSwaps() {
+    swaps_.clear();
+
+    // TODO: replace by a more efficient implementation
+    for (int i = 0; i < data_.size(); ++i)
+      for (int j = i + 1; j < data_.size(); ++j) {
+        if (data_[i] > data_[j]) {
+          swap(data_[i], data_[j]);
+          swaps_.push_back(make_pair(i, j));
+        }
+      }
+  }
+
+ public:
+  void Solve() {
+    ReadData();
+    GenerateSwaps();
+    WriteResponse();
+  }
+};
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  HeapBuilder heap_builder;
+  heap_builder.Solve();
+  return 0;
+}
