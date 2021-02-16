51d9eeed6e61320fa5aa7533d20e89a1f3ceddb1
eltoncyc <mreltonchan@gmail.com>
2021-02-15 22:46:06 -0800

    complete heap builder

--- Week_2/build_heap.cpp
+++ Week_2/build_heap.cpp
@@ -29,17 +29,54 @@ class HeapBuilder {
       cin >> data_[i];
   }
 
+  int parent(int child) {
+      return int((child + 1) / 2);
+  }
+
+
+  int left_child(int parent) {
+      return 2 * parent + 1;
+  }
+
+
+  int right_child(int parent) {
+      return 2 * parent + 2;
+  }
+
+  void down(int i)
+  {
+      int min_index = i;
+
+      int left_index = left_child(i);
+      int right_index = right_child(i);
+
+      if ((left_index < data_.size()) && (data_[left_index] < data_[min_index]))
+          min_index = left_index;
+
+      if ((right_index < data_.size()) && (data_[right_index] < data_[min_index]))
+          min_index = right_index;
+
+      if (min_index != i) {
+
+          swap(data_[i], data_[min_index]);
+          swaps_.push_back(make_pair(i, min_index));
+          down(min_index);
+
+      }
+  }
+
   void GenerateSwaps() {
     swaps_.clear();
 
     // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
-        }
-      }
+    for (int i = int(data_.size() / 2); i >= 0; i++)
+    {   
+       
+        down(i);
+       
+    }
+
+
   }
 
  public:
